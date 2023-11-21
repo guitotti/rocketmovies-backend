a33a3ad7d0e4f393bfbcb8b9b29e54fc51a60173
Guilherme Totti <dev.guilhermetotti@gmail.com>
2023-11-20 22:37:11 -0300

    adicionando rota index de movie_notes

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -35,6 +35,48 @@ class NotesController {
 
     return response.json();
   }
+
+  async index(request, response) {
+    const { title, user_id, movie_tags } = request.query;
+
+    let movie_notes;
+
+    if (movie_tags) {
+      const filteredTags = movie_tags.split(',').map(tag => tag.trim());
+
+      movie_notes = await knex("movie_tags")
+      .select([
+        "movie_notes.id",
+        "movie_notes.title",
+        "movie_notes.user_id"
+      ])
+      .where("movie_notes.user_id", user_id)
+      .whereLike("movie_notes.title", `%${title}%`)
+      .whereIn("name", filteredTags)
+      .innerJoin("movie_notes", "movie_notes.id", "movie_tags.movie_note_id")
+      .orderBy("movie_notes.title")
+
+    } else {
+      movie_notes = await knex("movie_notes")
+        .where({user_id})
+        .whereLike("title", `%${title}%`)
+        .orderBy("title");
+
+      return response.json(movie_notes);
+    }
+
+    const userMovieTags = await knex("movie_tags").where({ user_id });
+    const movieNotesWithTags = movie_notes.map(movie_note => {
+      const movieNoteTags = userMovieTags.filter(movie_tag => movie_tag.note_id === movie_note.id);
+
+      return {
+        ...movie_note,
+        movie_tags: movieNoteTags
+      }
+    });
+
+    return response.json(movieNotesWithTags);
+  }
 }
 
 module.exports = NotesController;
--- src/routes/movie_notes.routes.js
+++ src/routes/movie_notes.routes.js
@@ -6,6 +6,7 @@ const movieNotesRoutes = Router();
 
 const movieNotesController = new MovieNotesController();
 
+movieNotesRoutes.get("/", movieNotesController.index);
 movieNotesRoutes.post("/:user_id", movieNotesController.create);
 movieNotesRoutes.get("/:id", movieNotesController.show);
 movieNotesRoutes.delete("/:id", movieNotesController.delete);
